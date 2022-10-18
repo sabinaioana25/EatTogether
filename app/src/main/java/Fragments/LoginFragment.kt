f0c714f04bc1a71f80ef1b3f3f9d727af1787248
Sabina Ioana Leitoiu <sabina.leitoiu@masabi.com>
2022-10-18 20:10:55 +0100

    App Initial Structure: - build rough structure of the app - added fragment activities and layouts

---
+++ app/src/main/java/Fragments/LoginFragment.kt
@@ -0,0 +1,30 @@
+package Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.eattogether.R
+import com.example.eattogether.databinding.FragmentLoginBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false
+        )
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/example/eattogether/MainActivity.kt
+++ app/src/main/java/com/example/eattogether/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.eattogether
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.eattogether.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil
+            .setContentView<ActivityMainBinding>(
+                this, R.layout.activity_main)
     }
+
 }
