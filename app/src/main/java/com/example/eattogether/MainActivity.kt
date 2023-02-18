bfbd59c7bae90108c04aa0967c1893870623fd94
Sabina <sabinaleitoiu91@gmail.com>
2023-02-18 14:35:29 +0000

    Feat: add custom toolbar

--- app/src/main/java/com/example/eattogether/MainActivity.kt
+++ app/src/main/java/com/example/eattogether/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.*
 import androidx.navigation.*
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.fragment.*
+import androidx.navigation.ui.*
 import com.example.eattogether.databinding.ActivityMainBinding
 import com.example.eattogether.utils.*
 import timber.log.*
@@ -13,35 +15,43 @@ import timber.log.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.plant(Timber.DebugTree())
 
-        @Suppress("UNUSED_VARIABLE")
-        binding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = setContentView(this, R.layout.activity_main)
+        binding.apply {
+            setSupportActionBar(toolbarMain)
+            navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+                .navController
+            NavigationUI.setupActionBarWithNavController(this@MainActivity, navController)
+            appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        val navController =
-            this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(
-            this, navController
-        )
+            navController.addOnDestinationChangedListener { _: NavController, nd: NavDestination, _: Bundle? ->
+                when (nd.id) {
+                    R.id.loginFragment -> {
+                        supportActionBar?.setDisplayShowTitleEnabled(true)
+                    }
+                    R.id.signupFragment -> {
+                        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    }
+                    R.id.mapFragment -> {
+                        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                    }
+                    else -> toolbarTitle.text = ""
+                }
+                supportActionBar?.setDisplayShowTitleEnabled(true)
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController =
-            this.findNavController(R.id.navHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(
+            navController,
+            appBarConfiguration
+        ) || super.onSupportNavigateUp()
     }
-
-//    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-//        if (ev != null) {
-//            currentFocus?.removeFocusAndHideKeyboard(
-//                this,
-//                ev
-//            )
-//        }
-//        return super.dispatchTouchEvent(ev)
-//    }
 }
